790f3e6d9bacb80524174cfffb51c567ece07dea
Ikechukwu Kenneth Nzekwe <inzekwe@c3r10p1.42sp.org.br>
2026-06-16 19:44:34 -0300

    ft_memmove.c

---
+++ ft_memmove.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inzekwe <inzekwe@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/16 19:42:57 by inzekwe           #+#    #+#             */
+/*   Updated: 2026/06/16 19:43:34 by inzekwe          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char		*d;
+	const unsigned char	*s;
+
+	if (!dest && !src)
+		return (NULL);
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	if (d > s)
+	{
+		while (n--)
+		{
+			d[n] = s[n];
+		}
+	}
+	else
+	{
+		ft_memcpy(dest, src, n);
+	}
+	return (dest);
+}
