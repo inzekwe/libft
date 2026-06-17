9cc80cb3a2c5e3f2cb0765d8b1798fdcdc8164cf
Ikechukwu Kenneth Nzekwe <inzekwe@c3r10p1.42sp.org.br>
2026-06-16 21:03:44 -0300

    ft_strdup.c

---
+++ ft_strdup.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inzekwe <inzekwe@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/16 21:03:05 by inzekwe           #+#    #+#             */
+/*   Updated: 2026/06/16 21:03:13 by inzekwe          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strdup(const char *s1)
+{
+	char	*dest;
+	size_t	len;
+
+	len = ft_strlen(s1);
+	dest = (char *)malloc(sizeof(char) * (len + 1));
+	if (!dest)
+		return (NULL);
+	ft_memcpy(dest, s1, len + 1);
+	return (dest);
+}
